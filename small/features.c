tarantool/small

commit e1e9fe1fd7a696b3f04b1481b5a0b29343372dec

Fix out of source tarantool build
Moved generated config.h to a binary directory, added it to include
paths for the library itself.

Don't sure how to 'export' this header to make it visible by tarantool.
The workaround I have is described below.

Removed config.h include from small/slab_arena.h because a tarantool
test fails to build in the following way:

 | In file included from /home/alex/p/tarantool-meta/tarantool/src/lib/small/small/slab_cache.h:38,
 |                  from /home/alex/p/tarantool-meta/tarantool/src/lib/small/small/region.h:39,
 |                  from /home/alex/p/tarantool-meta/tarantool/src/lib/core/memory.h:33,
 |                  from /home/alex/p/tarantool-meta/tarantool/test/unit/heap_iterator.c:6:
 | /home/alex/p/tarantool-meta/tarantool/src/lib/small/small/slab_arena.h:36:10: fatal error: config.h: No such file or directory
 |  #include "config.h"
 |           ^~~~~~~~~~

Added it instead to small/slab_arena.c.

I guess this problem may be fixed in a more proper way. A follow up
would be desirable.

This is the regression from 33a267d0ff32130e2fbb8b12b587384bc72538d0
('slab_arena: Enhance slab_arena_create to support madvise hints').

CMakeLists.txt
@@ -26,17 +26,20 @@ check_symbol_exists(MAP_ANONYMOUS sys/mman.h TARANTOOL_SMALL_HAVE_MAP_ANONYMOUS)
 check_function_exists(madvise TARANTOOL_SMALL_HAVE_MADVISE)
 check_symbol_exists(MADV_DONTDUMP sys/mman.h TARANTOOL_SMALL_HAVE_MADV_DONTDUMP)
 
+set(config_h "${CMAKE_CURRENT_BINARY_DIR}/small/include/config.h")
 configure_file(
     "small/config.h.cmake"
-    "small/config.h"
+    "${config_h}"
     )
 message (STATUS "")
+
+include_directories("${CMAKE_CURRENT_BINARY_DIR}/small/include")
 
 # Valgrind
 include_directories(third_party)
 
 set(lib_headers
-    small/config.h
+    "${config_h}"
     small/features.h
     small/ibuf.h
     small/lf_lifo.h

small/slab_arena.c
@@ -29,6 +29,7 @@
  * SUCH DAMAGE.
  */
 #include "slab_arena.h"
+#include "config.h"
 #include "quota.h"
 #include <stdio.h>
 #include <stdint.h>

small/slab_arena.h
@@ -33,7 +33,6 @@
 #include "lf_lifo.h"
 #include <sys/mman.h>
 #include <limits.h>
-#include "config.h"
 
 #if defined(__cplusplus)
 extern "C" {

commit 020716d8a1337118a2afbfaaf4bd13210e640d5c

Rename generated config.h to small_config.h
It appears that small/config.h can clash with tarantool's
trivia/config.h with in-source build (when it is configured with `cmake
. <...>` from a tarantool's source directory). So core file stripping
feature becomes broken and slab_arena test fails.

 | [019] Test failed! Result content mismatch:
 | [019] --- small/slab_arena.result	Mon May 20 21:37:46 2019
 | [019] +++ small/slab_arena.reject	Mon May 20 21:47:01 2019
 | [019] @@ -23,3 +23,4 @@
 | [019]  arena->maxalloc = 2000896
 | [019]  arena->used = 0
 | [019]  arena->slab_size = 65536
 | [019] +ERROR: Expected dd flag on VMA address 0x7f3ec2080000

It is the regression from e1e9fe1fd7a696b3f04b1481b5a0b29343372dec ('Fix
out of source tarantool build').

I still think we can fix all that problems in more appropriate way. Please,
consider this patch as workaround.

.gitignore
@@ -17,4 +17,4 @@ build/
 CTestTestfile.cmake
 Testing
 CTestTestfile.cmake
-small/config.h
+small/include/small_config.h

CMakeLists.txt
@@ -26,9 +26,9 @@ check_symbol_exists(MAP_ANONYMOUS sys/mman.h TARANTOOL_SMALL_HAVE_MAP_ANONYMOUS)
 check_function_exists(madvise TARANTOOL_SMALL_HAVE_MADVISE)
 check_symbol_exists(MADV_DONTDUMP sys/mman.h TARANTOOL_SMALL_HAVE_MADV_DONTDUMP)
 
-set(config_h "${CMAKE_CURRENT_BINARY_DIR}/small/include/config.h")
+set(config_h "${CMAKE_CURRENT_BINARY_DIR}/small/include/small_config.h")
 configure_file(
-    "small/config.h.cmake"
+    "small/small_config.h.cmake"
     "${config_h}"
     )
 message (STATUS "")

small/features.c
@@ -39,7 +39,7 @@
 #include <sys/mman.h>
 
 #include "features.h"
-#include "config.h"
+#include "small_config.h"
 
 typedef bool (*rt_helper_t)(void);
 

small/slab_arena.c
@@ -29,7 +29,7 @@
  * SUCH DAMAGE.
  */
 #include "slab_arena.h"
-#include "config.h"
+#include "small_config.h"
 #include "quota.h"
 #include <stdio.h>
 #include <stdint.h>