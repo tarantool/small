bf01e83bf8e9b12b9e4db1bc6d65dd613b5c1a90
mechanik20051988 <mechanik20.05.1988@gmail.com>
2021-02-27 13:21:40 +0300

    small: remove unused functions
    Removed unused functions, which depended on the tarantool code.
    Small is independent component, but these functions did not allow
    it to be used in the c++ code. Also, these functions are not used
    in the tarantool code too, so they can be removed painlessly.

--- include/small/small.h
+++ include/small/small.h
@@ -216,23 +216,6 @@ small_stats(struct small_alloc *alloc,
 
 #if defined(__cplusplus)
 } /* extern "C" */
-#include "exception.h"
-
-static inline void *
-smalloc_xc(struct small_alloc *alloc, size_t size, const char *where)
-{
-	void *ptr = smalloc(alloc, size);
-	if (ptr == NULL)
-		tnt_raise(OutOfMemory, size, "slab allocator", where);
-	return ptr;
-}
-
-static inline void *
-smalloc0_xc(struct small_alloc *alloc, size_t size, const char *where)
-{
-	return memset(smalloc_xc(alloc, size, where), 0, size);
-}
-
 #endif /* defined(__cplusplus) */
 
 #endif /* INCLUDES_TARANTOOL_SMALL_SMALL_H */
